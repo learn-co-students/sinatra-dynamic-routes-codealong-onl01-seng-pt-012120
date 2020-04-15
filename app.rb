da7c37f4550a957a43ba2f7d15281e2696bad032
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-15 18:11:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -20,9 +20,8 @@ class App < Sinatra::Base
   end
 
   get '/multiply/:num1/:num2' do
-    @num1 = params[:num]
-    @num2 = params[:num2]
-    "#{@num1} + #{@num2} = sum"
+    @product = params[:num1].to_i * params[:num2].to_i
+    	"#{@product}"
   end
 
 end
